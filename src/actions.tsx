4776d6aa4651ccb9bfc374b74c81a4be2bd4f290
NaeemInventionInnovation <muhammad-naeem@iinv.tech>
2025-02-05 13:16:10 +0500

    some work

--- src/actions.tsx
+++ src/actions.tsx
@@ -1,12 +1,6 @@
 "use server"
 
-import ImageKit from "imagekit";
-
-const imagekit = new ImageKit({
-  publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY!,
-  privateKey: process.env.PRIVATE_KEY!,
-  urlEndpoint: process.env.NEXT_PUBLIC_URL_ENDPOINT!,
-});
+import { imagekit } from "./utils";
 
 export const shareAction = async (
   formData: FormData,
@@ -31,7 +25,10 @@ export const shareAction = async (
     folder: "/posts",
     transformation: {
       pre: transformation
-    }
+    },
+    customMetadata: {
+      sensitive: settings.sensitive,
+    },
   },
     function (error, result) {
       if (error) console.log(error);
